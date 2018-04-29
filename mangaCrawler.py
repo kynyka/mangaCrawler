abf3e06a5adfeec2b525babf6ccfd2b1d06e9bd4
kynyka <congsu.yeh@gmail.com>
2018-04-29 12:29:19 +0800

    rewrite auto-rename

--- mangaCrawler.py
+++ mangaCrawler.py
@@ -18,11 +18,48 @@ def build_folder(p):
 def no_get(u):
     return int(re.match(reg1,u).group().strip(' -'))
 
+def dl(m, o , x, v):
+    for p in m:
+        img_name = re.findall(reg4, p)[0] # 原名
+        nose = img_name.split('.')[0]
+        tail = '.' + img_name.split('.')[1]  # extension
+        conj = re.findall(reg5, nose)
+        if len(conj) != 0:  # filter 55/54/53/52/45/44/43/42/35/34/33/32/22/23/24/25 type like '00181-0019.jpg', '0012-00193.png', '00071-00082.jpeg', '0009-0010.png', '08-00091.jpg', '0011-122.jpg', '0022_23.jpg', '10-11.png', '02-03.jpg' etc.
+            re_name = conj[0][0] + conj[0][1] + conj[0][2] + tail
+            urllib.urlretrieve('https:' + p, path + x + '/' + re_name)
+            print '{} --> {}   OK --- br. 1'.format(img_name, re_name)
+        else:  # - or _ exclusive
+            if len(nose) == 2:  # 2-digit type I
+                urllib.urlretrieve('https:' + p, path + x + '/' + img_name)
+                print img_name, '  OK --- orig.'
+            elif len(nose) == 3 and re.search(r'[a-z]', nose) == None:  # 2-digit type II
+                re_name = nose[:2] + tail
+                urllib.urlretrieve('https:' + p, path + x + '/' + re_name)
+                print '{} --> {}   OK --- br. 2'.format(img_name, re_name)
+            elif len(nose) >= 4 and re.search(r'[a-z]', nose) == None:  # 00(00).png 00(07).jpg 00(21).png; exclude 0(05)1.png whose type's nonexistent by far
+                re_name = nose[2:4] + tail
+                urllib.urlretrieve('https:' + p, path + x + '/' + re_name)
+                print '{} --> {}   OK --- br. 3'.format(img_name, re_name)
+            else: # including 01a.jpg, 0001a.jpg
+                print img_name, '  OK --- Skip'
+                o += 1
+                continue
+        o += 1
+    print '\nDownload Counts: <{}>'.format(o)
+    if o == int(v):
+        print '【%s】/%s pages of 【%s】 -- Process End！\n\n' % (o, v, x)
+    else:
+        print u'エラーが発生しました'
+
 def get_dl(z, y, x, w):
-    print 'Yes, <{}> chapter(s) need to be downloaded!'.format(y)
+    print '【{}】 chapter(s) left!'.format(y)
     cd(z)
-    print '\nProcess Start...'
+    print '\n【{}】 -- Process Start...'.format(x)
     build_folder(path + x)
+
+    url_c_list = [] # 第一页chpt_lk_list[0]就不放进去了,因为能同时获得第一张图
+    img_src_list = []  ##上一行与这行移到函数中, 不然一次下几话的情况时会导致重复添加##
+
     count = 0
     r_new = urllib2.urlopen(urllib2.Request(url= w, headers=send_headers))
     html_c = r_new.read()
@@ -33,32 +70,9 @@ def get_dl(z, y, x, w):
     img_src_list.extend(img_src0) # 加入第一张图
     [url_c_list.append(w[0:-1] + str(n)) for n in xrange(2, int(last_pno) + 1)]
     [img_src_list.extend(re.findall(reg3, urllib2.urlopen(urllib2.Request(url=i, headers=send_headers)).read())) for i in url_c_list]
-    print 'Valid Image Url Numbers: <{}>\nLast Page Number: <{}>\n'.format(len(img_src_list), last_pno)
+    print 'Valid Image Url Counts: <{}>\nLast Page Number: <{}>\n'.format(len(img_src_list), last_pno)
     # print 'img_src_list: '+str(img_src_list)
-    for p in img_src_list: # start download
-        img_name = re.findall(reg4, p)[0] # 原名
-        if len(img_name.split('.')[0]) > 3 and (re.search('-|_', img_name) == None): # seldom '0000.jpg', '02_03.png', '02-03.jpg' etc. will disturb reg5 and ruin outcome
-            img_name = img_name[2:]
-        outcome = re.findall(reg5, img_name)
-        # print 'outcome: '+str(outcome)
-        if len(outcome) != 0:
-            if int(img_name[:2]) < 10:
-                img_rename = img_name[:2] + outcome[0][1]
-            elif 10 <= int(img_name[:2]) < 20:
-                img_rename = img_name[:2] + outcome[0][3]
-            else:
-                img_rename = img_name[:2] + outcome[0][5]
-            urllib.urlretrieve('https:' + p, path + x + '/' + img_rename)
-            print '{} --> {}  OK'.format(img_name, img_rename)
-        else:
-            urllib.urlretrieve('https:' + p, path + x + '/' + img_name)
-            print img_name, ' OK'
-        count += 1
-    print 'Download Counts: <{}>'.format(count)
-    if count == int(last_pno):
-        print '\n<%s>/%s pages of <%s> downloaded' % (count, last_pno, x)
-    else:
-        print u'エラーが発生しました'
+    dl(img_src_list, count, x, last_pno)
     update_log(x)
 
 def update_log(c):
@@ -67,8 +81,8 @@ def update_log(c):
         pickle.dump(c+'@'+ts, f)
 
 def cd(t):
-    while t > 0:
-        sys.stdout.write('\rTime break: {}s'.format(t))
+    while t >= 0: # 加了“=”
+        sys.stdout.write('\rTime lapse: {}s'.format(t))
         t -= 1
         sys.stdout.flush()
         time.sleep(1)
@@ -76,19 +90,16 @@ def cd(t):
 def do():
     with open(path+u'本地更至.txt','rb') as f1:
         local_cnt = pickle.loads(f1.read())
-    if chpt_no_latest - no_get(local_cnt) == 0:
-        print "Latest Chapter already Updated"
-    elif chpt_no_latest - no_get(local_cnt) == 1:
-        get_dl(5, 1, chpt[0][1], chpt_lk_list[0])
-    elif chpt_no_latest - no_get(local_cnt) == 2:
-        get_dl(5, 2, chpt[1][1], chpt_lk_list[1])
-        get_dl(5, 1, chpt[0][1], chpt_lk_list[0])
-    elif chpt_no_latest - no_get(local_cnt) >= 3:
-        get_dl(5, 3, chpt[2][1], chpt_lk_list[2])
-        get_dl(5, 2, chpt[1][1], chpt_lk_list[1])
-        get_dl(5, 1, chpt[0][1], chpt_lk_list[0])
+    chpt_no_local = no_get(local_cnt)
+    diff = chpt_no_latest - chpt_no_local
+    if diff == 0:
+        print "Latest Chapter Already!"
+    elif diff <= 4: ##目前站点上仅保留4话##
+        for d in xrange(diff):
+            get_dl(5, diff, chpt[diff-1][1], chpt_lk_list[diff-1])
+            diff -= 1
     else:
-        print "Make a check!\nlatest ONLINE No.: {}\n latest LOCAL No.: {}".format(*[chpt_no_latest, no_get(local_cnt)])
+        print "Make a check!\nlatest ONLINE No.: {}\n latest LOCAL No.: {}".format(*[chpt_no_latest, chpt_no_local])
     time.sleep(5)
     sys.exit()
 
@@ -115,14 +126,11 @@ if __name__ == "__main__":
         reg2 = re.compile(r'<li><a href="/r/shokugeki_no_souma/\d+/\d+/(\d+)">.+</a></li>')
         reg3 = re.compile(r'<img id="manga-page"\s{1,2}?src="(.+)"') # on 180218 site's html unexpectedly gives me 2 spaces
         reg4 = re.compile(r'//.+/\d+/\d+/(.+\.(?:jpg|png))') # non-capturing group, avoiding possible later .group(n)
-        reg5 = re.compile(r'(0\d(?:1|2))(\D{4,5})|(1\d(?:1|2))(\D{4,5})|(2\d(?:1|2))(\D{4,5})') # for instances like 012.png/031.jpeg/161.png/201.jpg, not fit for instances like 0001.jpg/0001a.png/0002.png
+        reg5 = re.compile(r'(?:00)?(\d{2})\d?(-|_)(?:00)?(\d{2})\d?')
 
         chpt = re.findall(reg, html_m) # 返回一个list,只含四物
         chpt_lk_list = ['https://readms.net' + chpt[0][0], 'https://readms.net' + chpt[1][0], 'https://readms.net' + chpt[2][0], 'https://readms.net' + chpt[3][0]]
         chpt_no_latest = no_get(chpt[0][1])
-
-        url_c_list = [] # 第一页chpt_lk_list[0]就不放进去了,因为能同时获得第一张图
-        img_src_list = []
 
         # 更新记录在,则读取更新话数
         if os.path.exists(path+u'本地更至.txt'):
